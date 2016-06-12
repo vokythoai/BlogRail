c16495e8fffc8c81927c8ef84f28dafe432d8fdb
VoKyThoai <thoaivk@nustechnology.com>
2016-06-12 11:40:06 +0700

    update root for app

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :articles
-  root "articles#index"
+  root "users#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
